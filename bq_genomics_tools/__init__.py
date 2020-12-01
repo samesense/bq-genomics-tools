aab89018464031c8ddbf576345915eda3e11c9ea
samesense <samesense@gmail.com>
2020-12-01 15:45:37 -0500

    :tada: Initial commit

---
+++ bq_genomics_tools/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""Tools for BigQuery variant annotations"""
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
